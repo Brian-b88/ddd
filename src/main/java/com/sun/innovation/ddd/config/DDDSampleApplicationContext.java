2eb9acd6aa99e24e22be7c449fe10d82fee29446
brain <154342189@qq.com>
2022-09-19 22:17:04 +0800

    add context

---
+++ src/main/java/com/sun/innovation/ddd/config/DDDSampleApplicationContext.java
@@ -0,0 +1,10 @@
+package com.sun.innovation.ddd.config;
+
+/**
+ * @author Rich Brain
+ * @version 1.0.0
+ * @description TODO
+ * @createTime 2022年09月19日 22:16:00
+ */
+public class DDDSampleApplicationContext {
+}
